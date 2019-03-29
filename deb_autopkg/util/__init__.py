dba06196d6ace883f26a05e6235c59567321c557
Enrico Weigelt, metux IT consult <info@metux.net>
2019-03-29 18:56:10 +0100

    deb_autopkg: util: helpers for list/dict to text transformation
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- deb_autopkg/util/__init__.py
+++ deb_autopkg/util/__init__.py
@@ -6,3 +6,11 @@ def get_attr_def(cnt, name, dflt = None):
         if cnt[name] is not None:
             return cnt[name]
     return dflt
+
+"""transform a list to printable text via given transform function"""
+def list2text(data, func):
+    return "\n".join(map(func, data))
+
+"""transform a dict to printable text via given transform function"""
+def dict2text(data, func):
+    return "\n".join(map(func, data.values()))
