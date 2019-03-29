cd75390fce768321beafeb48ad072d7c5e2eb677
Enrico Weigelt, metux IT consult <info@metux.net>
2019-03-29 18:55:21 +0100

    deb_autopkg: util: get_attr_def() give dflt parameter default None
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- deb_autopkg/util/__init__.py
+++ deb_autopkg/util/__init__.py
@@ -1,7 +1,7 @@
 __all__ = [ "log" ]
 
 """get attribute from container or default if not set"""
-def get_attr_def(cnt, name, dflt):
+def get_attr_def(cnt, name, dflt = None):
     if name in cnt:
         if cnt[name] is not None:
             return cnt[name]
