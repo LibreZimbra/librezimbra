99f4ba857dcf9d8b8231637c1d724b2f4bd60735
Enrico Weigelt, metux IT consult <info@metux.net>
2019-12-19 14:28:24 +0100

    deb_autopkg: cmd: helper for invalidating a package within given pool and target
    Adding a helper for invalidating an already built package within given pool
    and target, which also loads the given config and does everything in one shot.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- deb_autopkg/cmd.py
+++ deb_autopkg/cmd.py
@@ -19,8 +19,15 @@ def build_package(conffile, pkg):
 def clone_all(conffile):
     return Builder(load(conffile)).clone_all()
 
+def get_builder(conffile):
+    return Builder(load(conffile))
+
 def get_dut(conffile, name):
     return load(conffile).get_dut(name)
 
 def dut_exec(conffile, dutname, cmd = []):
     return get_dut(conffile, dutname).dut_exec(cmd)
+
+def pool_invalidate_target_package(conffile, poolname, pkgname, targetname):
+    get_builder(conffile).conf.get_pool(poolname).invalidate_target_package(pkgname, targetname)
+    return True
