025932367d5a21715839eafa0abf53bc6683d9c6
Enrico Weigelt, metux IT consult <info@metux.net>
2020-02-25 19:26:22 +0100

    metux/log: colorize log output
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- metux/log.py
+++ metux/log.py
@@ -1,13 +1,22 @@
 from sys import stderr
 
+color_normal = "\033[0;32;39m"
+color_yellow = "\033[1;32;33m"
+color_green  = "\033[1;32;40m"
+color_cyan   = "\033[1;32;36m"
+color_red    = "\033[1;32;91m"
+
+def _wr(prefix, color, text):
+    stderr.write("%s%5s%s %s\n" % (color, prefix, color_normal, text))
+
 def info(text):
-    stderr.write("INFO: "+text+"\n")
+    _wr("INFO:", color_green, text)
 
 def warn(text):
-    stderr.write("WARN: "+text+"\n")
+    _wr("WARN:", color_yellow, text)
 
 def err(text):
-    stderr.write("ERR:  "+text+"\n")
+    _wr("ERR:", color_red, text)
 
 def debug(text):
-    stderr.write("DBG:  "+text+"\n")
+    _wr("DBG:", color_cyan, text)
