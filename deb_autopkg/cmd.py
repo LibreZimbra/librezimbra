e7744ba4657507a0f8b3bdf82c3b9c8468a90e2c
Enrico Weigelt, metux IT consult <info@metux.net>
2020-02-27 13:24:22 +0100

    deb_autopkg: cmd: catch TaskFail exceptions and print error
    catch the TaskFail exception in cmdline frontend functions
    and print a convenient error message instead of the huge
    exception/stacktrace dump.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- deb_autopkg/cmd.py
+++ deb_autopkg/cmd.py
@@ -1,33 +1,57 @@
 from .builder import Builder
 from .conf import load
+from .util.task import TaskFail
+from metux.log import err
 
+import functools
+
+def cmdfunc(func):
+    @functools.wraps(func)
+    def wrapper_cmdfunc(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except TaskFail as ex:
+            err("[%s] %s" % (ex.get_task_name(), ex.get_message()))
+            return 127
+    return wrapper_cmdfunc
+
+@cmdfunc
 def build_pool(conffile, pool):
     return Builder(load(conffile)).build_pool(pool)
 
+@cmdfunc
 def upload_pool(conffile, pool):
     return Builder(load(conffile)).upload_pool(pool)
 
+@cmdfunc
 def deploy_pool(conffile, pool):
     return Builder(load(conffile)).deploy_pool(pool)
 
+@cmdfunc
 def build_all(conffile):
     return Builder(load(conffile)).build_all()
 
+@cmdfunc
 def build_package(conffile, pkg):
     return Builder(load(conffile)).build_package(pkg)
 
+@cmdfunc
 def clone_all(conffile):
     return Builder(load(conffile)).clone_all()
 
+@cmdfunc
 def get_builder(conffile):
     return Builder(load(conffile))
 
+@cmdfunc
 def get_dut(conffile, name):
     return load(conffile).get_dut(name)
 
+@cmdfunc
 def dut_exec(conffile, dutname, cmd = []):
     return get_dut(conffile, dutname).dut_exec(cmd)
 
+@cmdfunc
 def pool_invalidate_target_package(conffile, poolname, pkgname, targetname):
     get_builder(conffile).conf.get_pool(poolname).invalidate_target_package(pkgname, targetname)
     return True
