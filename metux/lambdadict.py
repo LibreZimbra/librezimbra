ff9433cefb5919d8da96c68f37065f980b1e1e28
Enrico Weigelt, metux IT consult <info@metux.net>
2020-03-09 09:13:19 +0100

    metux: lambdadict: add defaults support
    Adding an internal dict for defaults, where we can fallback to if a
    requested key is missing. The idea is strictly separating defaults
    from actually defined values, so we don't need to care about load
    ordering, can change defaults anytime or remove keys and so let the
    defaults take effect again.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- metux/lambdadict.py
+++ metux/lambdadict.py
@@ -2,16 +2,49 @@ from inspect import isfunction
 from collections import Mapping
 
 class LambdaDict(dict):
-    def __init__(self, d):
+    def __init__(self, d, dflt = {}):
         dict.__init__(self, d)
+        self.defaults = dict(dflt)
+
+    def __getitem_raw__(self, key):
+        if dict.has_key(self, key):
+            return dict.__getitem__(self, key)
+
+        if key in self.defaults:
+            return self.defaults[key]
+
+        return None
 
     def __getitem__(self, key):
-        item = dict.__getitem__(self, key)
+        item = self.__getitem_raw__(key)
 
-        if isfunction(item):
+        if callable(item):
             return item()
 
         if isinstance(item, Mapping):
             return LambdaDict(item)
 
         return item
+
+    def has_key(self, key):
+        if dict.has_key(self, key):
+            return True
+
+        if (self.defaults.has_key(key)):
+            return True
+
+        return False
+
+    """set a default value, which is returned when key not found"""
+    def default_set(self, key, value):
+        self.defaults[key] = value
+
+    """remove a default value"""
+    def default_del(self, key):
+        self.defaults.pop(key, None)
+
+    """add a list of default values"""
+    def default_addlist(self, attrs):
+        if attrs is not None:
+            for key in attrs:
+                self.defaults[key] = attrs[key]
