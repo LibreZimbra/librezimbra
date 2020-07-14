be4ec3341cf8af4b84bdc8753c242cfcdbf87edb
Enrico Weigelt, metux IT consult <info@metux.net>
2020-07-14 13:40:03 +0200

    deb_autopkg: util: drop unused functions
    The functions list2text() and dict2text() are unused for quite some
    time, so drop them.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- deb_autopkg/util/__init__.py
+++ deb_autopkg/util/__init__.py
@@ -1,12 +1,2 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) Enrico Weigelt, metux IT consult <info@metux.net>
-
-__all__ = [ "log" ]
-
-"""transform a list to printable text via given transform function"""
-def list2text(data, func):
-    return "\n".join(map(func, data))
-
-"""transform a dict to printable text via given transform function"""
-def dict2text(data, func):
-    return "\n".join(map(func, data.values()))
