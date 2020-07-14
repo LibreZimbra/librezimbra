550c7e4de3c327128320b1afaa2de215f953da99
Enrico Weigelt, metux IT consult <info@metux.net>
2020-07-14 13:17:04 +0200

    metux: drop obsolete function setexec()
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- metux/__init__.py
+++ metux/__init__.py
@@ -3,15 +3,9 @@
 
 from subprocess import call
 from os.path import abspath
-from os import chmod
-import os
-import stat
 
 def mkdir(dirname):
     call(['mkdir', '-p', abspath(dirname)])
 
 def rmtree(dirname):
     call(['rm', '-Rf', abspath(dirname)])
-
-def setexec(fn):
-    chmod(fn, os.stat(fn).st_mode | stat.S_IEXEC)
