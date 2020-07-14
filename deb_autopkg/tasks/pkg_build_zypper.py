66473f7e78dafdebb2440fec4bba999fcf1cd7df
Enrico Weigelt, metux IT consult <info@metux.net>
2020-07-14 11:45:06 +0200

    metux: move schroot into 'util' namespace
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- deb_autopkg/tasks/pkg_build_zypper.py
+++ deb_autopkg/tasks/pkg_build_zypper.py
@@ -6,7 +6,8 @@ from os import getuid, getgid
 from os.path import abspath
 from subprocess import call, Popen, PIPE
 from glob import glob
-from metux import rmtree, mkdir, schroot
+from metux import rmtree, mkdir
+from metux.util import schroot
 import shutil
 
 #
