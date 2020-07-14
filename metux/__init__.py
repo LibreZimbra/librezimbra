0ab827d14b314d4a1c3e5decbf25d31a5ee694d7
Enrico Weigelt, metux IT consult <info@metux.net>
2020-07-14 13:11:26 +0200

    metux: drop unused function extract_tar()
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- metux/__init__.py
+++ metux/__init__.py
@@ -13,22 +13,5 @@ def mkdir(dirname):
 def rmtree(dirname):
     call(['rm', '-Rf', abspath(dirname)])
 
-def extract_tar(tarball, tempdir):
-    tarball = abspath(tarball)
-    tempdir = abspath(tempdir)
-    rmtree(tempdir)
-    mkdir(tempdir)
-
-    if (tarball.endswith(".tar.gz")):
-        return call(['tar', '-xzf', abspath(tarball), '-C', tempdir])
-    elif (tarball.endswith(".tar")):
-        return call(['tar', '-xf', abspath(tarball), '-C', tempdir])
-    elif (tarball.endswith(".tar.bz2")):
-        return call(['tar', '-xjf', abspath(tarball), '-C', tempdir])
-    elif (tarball.endswith(".zip")):
-        return call(['unzip', abspath(tarball), '-d', tempdir])
-    else:
-        raise BaseException("unknown format: "+tarball)
-
 def setexec(fn):
     chmod(fn, os.stat(fn).st_mode | stat.S_IEXEC)
