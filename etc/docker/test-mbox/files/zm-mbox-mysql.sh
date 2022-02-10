1234d29702bd9601d1c58797fc23d52f93e8233c
Enrico Weigelt, metux IT consult <info@metux.net>
2022-02-10 16:00:10 +0100

    1

--- etc/docker/test-mbox/files/zm-mbox-mysql.sh
+++ etc/docker/test-mbox/files/zm-mbox-mysql.sh
@@ -11,7 +11,8 @@ zm_mbox_mysql_init() {
 
     if [ ! -d $ZM_MYSQL_DATADIR/mysql ]; then
         zm_log_info "need to initialize mysql database"
-        zm_runas mysql_install_db --defaults-file=/opt/zimbra/conf/my.cnf --datadir=$ZM_MYSQL_DATADIR
+#        zm_runas mysql_install_db --defaults-file=/opt/zimbra/conf/my.cnf --datadir=$ZM_MYSQL_DATADIR
+        zm_runas mysql_install_db --user=zimbra --datadir=$ZM_MYSQL_DATADIR
     else
         zm_log_info "mysql already initialized"
     fi
